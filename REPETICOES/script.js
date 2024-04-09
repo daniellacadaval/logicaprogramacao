40c8eb99bdab9edc8e4063658de42a50e74c0595
Daniella Cadaval <150399707+daniellacadaval@users.noreply.github.com>
2024-04-08 22:09:05 -0300

    Exercícios WHILE - Rafael
    Novas práticas ensinadas e aplicadas durante a aula

---
+++ REPETICOES/script.js
@@ -0,0 +1,69 @@
+
+// let saida = document.getElementById("divSaida")
+// console.log(saida);
+
+// //saida.innerHTML = "Olá, otário!"
+
+// // PORTUGOL
+// //enquanto(){}
+
+// let cont = 0
+// while(cont < 10){
+//     //console.log(cont);
+//     saida.innerHTML += cont + " patinho foi passear... <br>"
+//     cont++
+// }
+
+// while(cont > 0){
+//     saida.innerHTML += cont = " patinho voltaram de lá <br>"
+//     cont--
+// }
+
+//EXERCICIO 1
+// let cont = 1
+// while(cont <= 10){
+//     console.log(cont ** 2);
+//     cont++
+// }
+
+//EXERCICIO 2
+// let cont = 10
+// while(cont >= 0){
+//     console.log(cont);
+//     cont--
+// }
+
+//EXERCICIO 3
+// let cont = 1
+// let soma = 0
+// while(cont <= 100){
+    
+//     soma += cont
+//     cont++
+//     console.log(soma);
+// }
+
+//EXERCICIO 4
+// let contador = 1
+// while(contador <= 10){
+//     console.log(contador * 5);
+//     contador++
+// }
+
+//EXERCICIO 5
+// let contador = 1
+// let tabuada = Number(prompt("Digite um número para o programa mostrar a tabuada:"))
+
+// while(contador <= 10){
+//     console.log(contador * tabuada);
+//     contador++
+// }
+
+//EXERCICIO 6
+
+// let contador = 1
+// while(contador <= 20){
+//     if(contador % 2 == 1)
+//     console.log(contador);
+//     contador++
+// }
