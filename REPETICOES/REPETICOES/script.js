83d61390305a0e42558d1b56bdac2d5cd30c75d2
Daniella Cadaval <150399707+daniellacadaval@users.noreply.github.com>
2024-04-15 21:54:52 -0300

    Atualização dos exercícios de repetição (parte 1)

---
+++ REPETICOES/REPETICOES/script.js
@@ -0,0 +1,113 @@
+
+// let saida = document.getElementById("divSaida")
+// console.log(saida);
+
+// //saida.innerHTML = "Olá, otário!"
+
+// // PORTUGOL
+// //enquanto(){}
+
+// let cont = 0
+// while(cont < 10){
+//     //console.log(cont);
+//     saida.innerHTML += cont + " patinho foi passear... <br>"
+//     cont++
+// }
+
+// while(cont > 0){
+//     saida.innerHTML += cont = " patinho voltaram de lá <br>"
+//     cont--
+// }
+
+//EXERCICIO 1
+// let cont = 1
+// while(cont <= 10){
+//     console.log(cont ** 2);
+//     cont++
+// }
+
+//EXERCICIO 2
+// let cont = 10
+// while(cont >= 0){
+//     console.log(cont);
+//     cont--
+// }
+
+//EXERCICIO 3
+// let cont = 1
+// let soma = 0
+// while(cont <= 100){
+
+//     soma += cont
+//     cont++
+//     console.log(soma);
+// }
+
+//EXERCICIO 4
+// let contador = 1
+// while(contador <= 10){
+//     console.log(contador * 5);
+//     contador++
+// }
+
+//EXERCICIO 5
+// let contador = 1
+// let tabuada = Number(prompt("Digite um número para o programa mostrar a tabuada:"))
+
+// while(contador <= 10){
+//     console.log(contador * tabuada);
+//     contador++
+// }
+
+//EXERCICIO 6
+
+// let contador = 1
+// while(contador <= 20){
+//     if(contador % 2 == 1)
+//     console.log(contador);
+//     contador++
+// }
+
+//EXERCÍCIO 7
+
+// let contador = 1
+// while (contador <= 10) {
+//         console.log(contador)
+//         contador++
+// }
+// contador = 10
+// while (contador >= 1) {
+//         console.log(contador)
+//         contador--
+// }
+
+//EXERCÍCIO 8
+
+
+// let numero = Number(prompt("Digite um numero:"))
+// let fatorial = 1
+// while(numero >= 1){
+//         fatorial *= numero
+//         console.log(fatorial);
+//         numero--
+// }
+
+//EXERCÍCIO 9
+
+// let contador = 1
+// while(contador <= 100){
+//         if(contador % 3 == 0 && contador % 5 == 0){
+//                 console.log("FizzBuzz");
+//         }else if(contador % 3 == 0){
+//                 console.log("Fizz");
+//         }else if(contador % 5 == 0){
+//                 console.log("Buzz");
+//         }else{
+//                 console.log(contador);
+//         }
+//         contador++
+// }
+
+//EXERCÍCIO 10
+
+
